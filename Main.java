12581cd3c46536a1f775a5cfdef5f9f7cf96f788
Bob283527 <jiangjon27@7hills.org>
2025-10-05 19:15:29 -0400

    Start the Farkle game

---
+++ Main.java
@@ -0,0 +1,12 @@
+//Jon Jiang
+//Twyford 27
+//CSA
+//10/05/2025
+
+public class Main {
+   //starts the gane
+    public static void main(String[] args) {
+        Farkle game = new Farkle();
+        game.playGame();
+    }
+}
